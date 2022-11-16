cbfc161eec50a3efc2275533afb7c15cae374f54
V Prajwal <VJJ@kmd.dk>
2022-11-17 00:50:56 +0530

    react new components added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
   //const expenseDate = new Date(2022, 11, 16);
   //const expenseTitle = "Car Insurance";
   //const expenseAmount = 2000;
+
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
